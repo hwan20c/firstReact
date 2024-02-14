18e3674c0d6d2457c88edb520e24323dea530c21
hwan20c <hwan20c@gmail.com>
2024-02-14 11:55:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
@@ -17,7 +19,18 @@ function App() {
       element: <Home />,
     },
   ]);
-  return <RouterProvider router={router} />;
+
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <RouterProvider router={router}>
+        <Routes>
+          <Route path="/hello" element={<h1>hello</h1>} />
+          <Route path="/movie/:id" element={<Detail />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </RouterProvider>
+    </Router>
+  );
 }
 
 export default App;
