9be66bfb8a174373a8099612bcf86df147ee0929
hwan20c <hwan20c@gmail.com>
2024-02-15 11:52:09 +0900

    app update

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/hello",
-      element: <h1>hello</h1>,
-    },
-    {
-      path: "/movie/:id",
-      element: <Detail />,
-    },
-    {
-      path: "/",
-      element: <Home />,
-    },
-  ]);
-
   return (
     <Router basename={process.env.PUBLIC_URL}>
-      <RouterProvider router={router}>
-        <Routes>
-          <Route path="/hello" element={<h1>hello</h1>} />
-          <Route path="/movie/:id" element={<Detail />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </RouterProvider>
+      <Routes>
+        <Route path="/hello" element={<h1>hello</h1>} />
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
     </Router>
   );
 }
